1ac78ec718c81045b3c170617e9be2dd930cc631
erlm <erlm@kea.dk>
2023-04-14 10:37:06 +0200

    start på pagination nu med sorting

--- src/main/java/com/example/regionkommunev1/controller/PageController.java
+++ src/main/java/com/example/regionkommunev1/controller/PageController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -82,6 +84,47 @@ public class PageController {
         return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
+
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
+        return Sort.Direction.ASC;
+    }
+
+    @GetMapping("kommunepagesort")
+    public ResponseEntity<Map<String, Object>> getCountyPageAndSort(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "kode,desc") String[] sort) {
+
+        List<Sort.Order> orders = new ArrayList<>();
+        if (sort[0].contains(",")) {
+            for (String sortOrder: sort) {
+                String[] _sort = sortOrder.split(",");
+                orders.add(new Sort.Order(getSortDirection(_sort[1]), _sort[0]));
+            }
+        } else {
+            orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
+        }
+
+        Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
+        Page<Kommune> pageCounty = kommuneRepository.findAll(pagingSort);
+        List<Kommune> county = pageCounty.getContent();
+
+        if (county.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("county", county);
+        response.put("currentPage", pageCounty.getNumber());
+        response.put("totalItems", pageCounty.getTotalElements());
+        response.put("totalPages", pageCounty.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
 
 }
 
