36e07136661ed0d13d4a58fb74b37fe3ac3f4767
erlm <erlm@kea.dk>
2023-04-21 14:29:47 +0200

    start på pagination nu med sorting

--- src/test/java/com/example/regionkommunev1/service/ServiceKommunerImplTest.java
+++ src/test/java/com/example/regionkommunev1/service/ServiceKommunerImplTest.java
@@ -2,6 +2,8 @@ package com.example.regionkommunev1.service;
 
 import com.example.regionkommunev1.model.Kommune;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -25,6 +27,19 @@ class ServiceKommunerImplTest {
     void getKommunerStartsWith0() {
         List<Kommune> kmns = serviceKommuner.getKommunerStartsWith('r');
         assertEquals(0, kmns.size());
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(chars = {'B', 'D', 'G', 'R','K'}) // five letters
+    void testLastDigitParam(char letter) {
+        List<Kommune> kommuner = serviceKommuner.getKommunerStartsWith(letter);
+        assertNotEquals(0, kommuner.size());
+        assertEquals(true, kommuner.size()<100);
+        Kommune kommune = kommuner.get(0);
+        String str = kommune.getName();
+        char firstLetter = str.substring(0,1).charAt(0);
+        assertEquals(letter, firstLetter);
     }
 
 }
